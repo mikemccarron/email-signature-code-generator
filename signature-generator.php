6c39700c7e150f8ec86cf7b05120893fccf18c25
Mike McCarron <mike@mikemccarron.com>
2021-02-13 11:43:39 -0500

    updated sample assets

--- signature-generator.php
+++ signature-generator.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>1TP HTML Signature Generator</title>
+        <title>HTML Signature Generator</title>
         <style>
             body {
               font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
